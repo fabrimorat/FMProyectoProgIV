a87972a5784a17f97e50dc6344830fe7b09c8c4d
fabrimorat <82410555+fabrimorat@users.noreply.github.com>
2022-01-02 22:37:00 -0500

    Create FabricioMora.cs

---
+++ FabricioMora.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoProgIVUWP
+{
+    class FabricioMora
+    {
+        public int Codigo { get; set; }
+
+
+        public string NombreModelo { get; set; }
+
+
+        public string Autonomia { get; set; }
+
+
+        public DateTime FechaFabricacion { get; set; }
+
+
+        public string Velocidad { get; set; }
+    }
+}
